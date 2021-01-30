4f848a0de0305f02b7a07ad39573329b8a67cb3a
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2021-01-30 15:59:00 +0100

    iteration 1 - adding place model

---
+++ models/place.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  type: {
+    type: String,
+    enum: ['coffee_shop', 'bookstore']
+  },
+  timestamps: true
+});
