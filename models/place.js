2a9f2c67a7f937e8fa4363b38d5c50f1a2a23ab5
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2021-01-30 16:45:37 +0100

    iteration 2 - adding views and post handler for creating place

--- models/place.js
+++ models/place.js
@@ -14,3 +14,4 @@ const schema = new mongoose.Schema(
     timestamps: true
   }
 );
+module.exports = mongoose.model('Place', schema);
--- routes/places.js
+++ routes/places.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const router = new express.Router();
-
 const Place = require('./../models/place');
 
 // get handler for creating new place
@@ -11,5 +9,31 @@ router.get('/create-place', (req, res, next) => {
 });
 
 // handling post request for creating new place
+router.post('/create-place', (req, res, next) => {
+  const data = req.body;
+
+  Place.create({
+    name: data.name,
+    type: data.type
+  })
+    .then((place) => {
+      res.redirect(`/place/${place._id}`);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// handling get request for viewing single place
+router.get('place/:id', (req, res, next) => {
+  const id = req.params.id;
+  Place.findById(id)
+    .then((place) => {
+      res.render('places/details', { place });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 
 module.exports = router;
